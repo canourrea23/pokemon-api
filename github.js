9303342c68c9a8ab04bf8b190da2bb6720444c95
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-10-28 10:36:48 -0500

    [github] Add new object and element page

--- github.js
+++ github.js
@@ -1,9 +1,22 @@
+const container = document.querySelector('.container');
+
+
 fetch('https://api.github.com/users/canourrea23')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const canoObj = {
+        bio: githubData.bio,
+        username:githubData.login,
+        name: githubData.name
+    }
+        console.log(githubData);
+
+        const newElement = document.createElement('p');
+        newElement.textContent = canoObj.name;
+        // append newElement to container
+        container.appendChild(newElement);
 })
 
 .catch(error => {
